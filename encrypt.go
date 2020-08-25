84fbfc9ad82611197c2fc74719cfcbaddd4c4667
倒霉狐狸 <mail@xiaoliu.org>
2020-08-25 22:22:39 +0800

    添加rsa加密

--- encrypt.go
+++ encrypt.go
@@ -12,8 +12,13 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/hex"
+	"encoding/pem"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -78,8 +83,9 @@ func PasswordVerify(hashedPwd string, plainPwd string) bool {
 	return true
 }
 
-// EncyptogAES
 // ---------------DES加密  解密--------------------
+
+// EncyptogAES AES加密
 func EncyptogAES(src, key string) string {
 	s := []byte(src)
 	k := []byte(key)
@@ -99,7 +105,7 @@ func EncyptogAES(src, key string) string {
 
 }
 
-// DecrptogAES
+// DecrptogAES AES解密
 func DecrptogAES(src, key string) string {
 	s := []byte(src)
 	k := []byte(key)
@@ -112,4 +118,106 @@ func DecrptogAES(src, key string) string {
 	n := len(s)
 	count := int(s[n-1])
 	return string(s[:n-count])
+}
+
+// ---------------RSA 密钥对生成 加密 解密--------------------
+
+//生成RSA私钥和公钥，保存到文件中
+func GenerateRSAKey(bits int) {
+	//GenerateKey函数使用随机数据生成器random生成一对具有指定字位数的RSA密钥
+	//Reader是一个全局、共享的密码用强随机数生成器
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		panic(err)
+	}
+	//保存私钥
+	//通过x509标准将得到的ras私钥序列化为ASN.1 的 DER编码字符串
+	X509PrivateKey := x509.MarshalPKCS1PrivateKey(privateKey)
+	//使用pem格式对x509输出的内容进行编码
+	//创建文件保存私钥
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer privateFile.Close()
+	//构建一个pem.Block结构体对象
+	privateBlock := pem.Block{Type: "RSA Private Key", Bytes: X509PrivateKey}
+	//将数据保存到文件
+	pem.Encode(privateFile, &privateBlock)
+
+	//保存公钥
+	//获取公钥的数据
+	publicKey := privateKey.PublicKey
+	//X509对公钥编码
+	X509PublicKey, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		panic(err)
+	}
+	//pem格式编码
+	//创建用于保存公钥的文件
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer publicFile.Close()
+	//创建一个pem.Block结构体对象
+	publicBlock := pem.Block{Type: "RSA Public Key", Bytes: X509PublicKey}
+	//保存到文件
+	pem.Encode(publicFile, &publicBlock)
+}
+
+//RSA加密
+func RSA_Encrypt(plainText []byte, path string) []byte {
+	//打开文件
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//读取文件的内容
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	//x509解码
+
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	//类型断言
+	publicKey := publicKeyInterface.(*rsa.PublicKey)
+	//对明文进行加密
+	cipherText, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, plainText)
+	if err != nil {
+		panic(err)
+	}
+	//返回密文
+	return cipherText
+}
+
+//RSA解密
+func RSA_Decrypt(cipherText []byte, path string) []byte {
+	//打开文件
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//获取文件内容
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	//X509解码
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	//对密文进行解密
+	plainText, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
+	//返回明文
+	return plainText
 }
